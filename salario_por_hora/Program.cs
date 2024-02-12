925f445fda8b65e7c85f020c75dabb76a8e1460f
Michael112007 <117554393+Michael112007@users.noreply.github.com>
2024-02-12 07:17:21 -0400

    Agregar archivos de proyecto.

---
+++ salario_por_hora/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("salario_por_horas");
+
+
+    {
+        double pagoPorHora, horasTrabajadas, sueldoBruto, descuentoAFP, descuentoSFS, totalDescuento, sueldoNeto;
+
+        Console.Write("Ingrese el pago por hora: ");
+        pagoPorHora = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese las horas trabajadas: ");
+        horasTrabajadas = Convert.ToDouble(Console.ReadLine());
+
+        sueldoBruto = pagoPorHora * horasTrabajadas;
+        descuentoAFP = sueldoBruto * 0.0287;
+        descuentoSFS = sueldoBruto * 0.0304;
+        totalDescuento = descuentoAFP + descuentoSFS;
+        sueldoNeto = sueldoBruto - totalDescuento;
+
+        Console.WriteLine("Sueldo bruto: " + sueldoBruto);
+        Console.WriteLine("Descuento AFP: " + descuentoAFP);
+        Console.WriteLine("Descuento SFS: " + descuentoSFS);
+        Console.WriteLine("Total descuento: " + totalDescuento);
+        Console.WriteLine("Sueldo neto: " + sueldoNeto);
+    }
+
